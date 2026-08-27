34c7067c87599a74559c1cee1e6636e90e2750f3
Dilsher10 <dilsher.dahri10@gmail.com>
2026-08-28 03:13:29 +0500

    changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@
 
 export default function Hero() {
     return (
-        <section className="hero card">
+        <section className="hero card hero-mobile">
             <div className="hero-copy"><h1>MADE FOR DIGITAL<br />MOMENTS</h1><p>Block More™ helps filter blue light during digital learning.</p><a className="btn" href="/product">SHOP NOW</a></div>
         </section>
     );
